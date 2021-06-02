c962af2f909980a67223a862462ae7b08c1b44ae
Dana Sugu <dana.sugu@gmail.com>
2021-06-02 08:07:08 +0300

    Update app.ts

--- app.ts
+++ app.ts
@@ -1,5 +1,7 @@
 import http from 'http';
 
-http.createServer((request, response) => {
-	response.end('Hello,World!');
-});
+http
+	.createServer((request, response) => {
+		response.end('Hello,World!');
+	})
+	.listen(3000, () => console.log('server started'));
